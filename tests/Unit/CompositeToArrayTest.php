661e69e09856c6e6d5fb834bf8cb3586731f3840
Alaa Hussein <alaaabueta@gmail.com>
2021-10-27 15:37:46 +0300

    Composite To Array Test

--- tests/Unit/CompositeToArrayTest.php
+++ tests/Unit/CompositeToArrayTest.php
@@ -2,12 +2,19 @@
 
 namespace Jawabkom\Backend\Module\Profile\Test\Unit;
 
+use Jawabkom\Backend\Module\Profile\Contract\IProfileComposite;
 use Jawabkom\Backend\Module\Profile\Contract\IProfileCriminalRecordEntity;
+use Jawabkom\Backend\Module\Profile\Contract\IProfileCriminalRecordRepository;
 use Jawabkom\Backend\Module\Profile\Contract\IProfileEmailEntity;
+use Jawabkom\Backend\Module\Profile\Contract\IProfileEmailRepository;
 use Jawabkom\Backend\Module\Profile\Contract\IProfileEntity;
 use Jawabkom\Backend\Module\Profile\Contract\IProfileMetaDataEntity;
+use Jawabkom\Backend\Module\Profile\Contract\IProfileMetaDataRepository;
 use Jawabkom\Backend\Module\Profile\Contract\IProfileNameEntity;
+use Jawabkom\Backend\Module\Profile\Contract\IProfileNameRepository;
+use Jawabkom\Backend\Module\Profile\Contract\IProfileRepository;
 use Jawabkom\Backend\Module\Profile\Contract\IProfileSkillEntity;
+use Jawabkom\Backend\Module\Profile\Contract\IProfileSkillRepository;
 use Jawabkom\Backend\Module\Profile\Contract\IProfileUuidFactory;
 use Jawabkom\Backend\Module\Profile\Mapper\ProfileCompositeToArrayMapper;
 use Jawabkom\Backend\Module\Profile\Service\SearchOfflineByFilters;
@@ -26,6 +33,8 @@ class CompositeToArrayTest extends AbstractTestCase
     private SearchOfflineByFilters $searchOfflineByFilters;
     private \Faker\Generator $faker;
     private IDependencyInjector $di;
+    private IProfileComposite $profileComposite;
+    private IProfileRepository $profileRepository;
 
     public function setUp(): void
     {
@@ -33,116 +42,126 @@ class CompositeToArrayTest extends AbstractTestCase
         $this->di = new DI();
         $this->createProfile = $this->di->make(CreateProfile::class);
         $this->searchOfflineByFilters = $this->di->make(SearchOfflineByFilters::class);
+        $this->profileComposite = $this->di->make(IProfileComposite::class);
+        $this->profileRepository = $this->di->make(IProfileRepository::class);
         $this->faker = Factory::create();
     }
 
-//    public function testMapProfileWithProfileIDToArray()
-//    {
-//        $dummyBasicData = $this->dummyBasicProfileData();
-//        $uuidFactory = $this->di->make(IProfileUuidFactory::class);
-//        /**@var $ProfileCompositeToArrayMapper ProfileCompositeToArrayMapper */
-//        $ProfileCompositeToArrayMapper = $this->di->make(ProfileCompositeToArrayMapper::class);
-//
-//        /**@var $IProfileEntity IProfileEntity */
-//        $IProfileEntity = $this->di->make(IProfileEntity::class);
-//        $IProfileEntity->setGender($dummyBasicData['gender'] ?? null);
-//        $IProfileEntity->setDataSource($dummyBasicData['data_source'] ?? null);
-//        $IProfileEntity->setPlaceOfBirth($dummyBasicData['place_of_birth'] ?? null);
-//        $IProfileEntity->setDateOfBirth(!empty($dummyBasicData['date_of_birth']) ? new \DateTime($dummyBasicData['date_of_birth']) : null);
-//        $IProfileEntity->setProfileId($uuidFactory->generate());
-//        $profileCompositeToArrayMapper = $ProfileCompositeToArrayMapper->mapProfile($IProfileEntity,true);
-//        $this->assertCount(5, $profileCompositeToArrayMapper);
-//    }
+    public function testMapProfileWithProfileIDToArray()
+    {
+        $uuidFactory = $this->di->make(IProfileUuidFactory::class)->generate();
+        $dummyBasicData = $this->dummyBasicProfileData();
+        $profile = $this->profileRepository->createEntity();
+        $profile->setGender($dummyBasicData['gender'] ?? null);
+        $profile->setDataSource($dummyBasicData['data_source'] ?? null);
+        $profile->setPlaceOfBirth($dummyBasicData['place_of_birth'] ?? null);
+        $profile->setDateOfBirth(!empty($dummyBasicData['date_of_birth']) ? new \DateTime($dummyBasicData['date_of_birth']) : null);
+        $profile->setProfileId($uuidFactory);
+        $this->profileComposite->setProfile($profile);
+
+        /**@var $ProfileCompositeToArrayMapper ProfileCompositeToArrayMapper */
+        $ProfileCompositeToArrayMapper = $this->di->make(ProfileCompositeToArrayMapper::class);
+
+        $profileCompositeToArrayMapper = $ProfileCompositeToArrayMapper->map($this->profileComposite,true);
+        $this->assertEquals($uuidFactory, $profileCompositeToArrayMapper['profile_id']);
+    }
 
 
-//    public function testMapEmailToArray()
-//    {
-//        $dummyEmailData = $this->dummyEmailsData();
-//
-//        /**@var $ProfileCompositeToArrayMapper ProfileCompositeToArrayMapper */
-//        $ProfileCompositeToArrayMapper = $this->di->make(ProfileCompositeToArrayMapper::class);
-//
-//        /**@var $IProfileEmailEntity IProfileEmailEntity */
-//        $IProfileEmailEntity = $this->di->make(IProfileEmailEntity::class);
-//        $IProfileEmailEntity->setValidSince(!empty($dummyEmailData['valid_since']) ? new \DateTime($dummyEmailData['valid_since']) : null);
-//        $IProfileEmailEntity->setEmail($dummyEmailData['email'] ?? null);
-//        $IProfileEmailEntity->setEspDomain($dummyEmailData['esp_domain'] ?? null);
-//        $IProfileEmailEntity->setType($dummyEmailData['type'] ?? null);
-//        $profileEmailFillArray = [];
-//        array_push($profileEmailFillArray, $IProfileEmailEntity);
-//        $profileCompositeToArrayMapper = $ProfileCompositeToArrayMapper->mapEmails($profileEmailFillArray);
-//        $this->assertCount(1, $profileCompositeToArrayMapper);
-//    }
+    public function testMapEmailToArray()
+    {
+        $dummyEmailData = $this->dummyEmailsData();
+        $profile = $this->profileRepository->createEntity();
 
-//    public function testMapSkillToArray()
-//    {
-//        $dummySkillData = $this->dummySkillsData();
-//        /**@var $ProfileCompositeToArrayMapper ProfileCompositeToArrayMapper */
-//        $ProfileCompositeToArrayMapper = $this->di->make(ProfileCompositeToArrayMapper::class);
-//
-//        /**@var $IProfileSkillEntity IProfileSkillEntity */
-//        $IProfileSkillEntity = $this->di->make(IProfileSkillEntity::class);
-//        $IProfileSkillEntity->setValidSince(!empty($dummySkillData['valid_since']) ? new \DateTime($dummySkillData['valid_since']) : null);
-//        $IProfileSkillEntity->setLevel($dummySkillData['level'] ?? null);
-//        $IProfileSkillEntity->setSkill($dummySkillData['skill'] ?? null);
-//        $profileSkillFillArray = [];
-//        array_push($profileSkillFillArray, $IProfileSkillEntity);
-//        $profileCompositeToArrayMapper = $ProfileCompositeToArrayMapper->mapSkills($profileSkillFillArray);
-//        $this->assertCount(1, $profileCompositeToArrayMapper);
-//    }
-//
-//    public function testMapCriminalRecordToArray()
-//    {
-//        $dummyCriminalRecordData = $this->dummyCriminalRecordsData();
-//        /**@var $ProfileCompositeToArrayMapper ProfileCompositeToArrayMapper */
-//        $ProfileCompositeToArrayMapper = $this->di->make(ProfileCompositeToArrayMapper::class);
-//
-//        /**@var $IProfileCriminalRecordEntity IProfileCriminalRecordEntity */
-//        $IProfileCriminalRecordEntity = $this->di->make(IProfileCriminalRecordEntity::class);
-//        $IProfileCriminalRecordEntity->setCaseNumber($dummyCriminalRecordData['case_number'] ?? null);
-//        $IProfileCriminalRecordEntity->setCaseType($dummyCriminalRecordData['case_type'] ?? null);
-//        $IProfileCriminalRecordEntity->setCaseYear($dummyCriminalRecordData['case_year'] ?? null);
-//        $IProfileCriminalRecordEntity->setCaseStatus($dummyCriminalRecordData['case_status'] ?? null);
-//        $IProfileCriminalRecordEntity->setDisplay($dummyCriminalRecordData['display'] ?? null);
-//        $profileCriminalRecordFillArray = [];
-//        array_push($profileCriminalRecordFillArray, $IProfileCriminalRecordEntity);
-//        $profileCompositeToArrayMapper = $ProfileCompositeToArrayMapper->mapCriminalRecords($profileCriminalRecordFillArray);
-//        $this->assertCount(1, $profileCompositeToArrayMapper);
-//    }
-//
-//    public function testMapMetaDataToArray()
-//    {
-//        $dummyMetaDataData = $this->dummyMetaData();
-//        /**@var $ProfileCompositeToArrayMapper ProfileCompositeToArrayMapper */
-//        $ProfileCompositeToArrayMapper = $this->di->make(ProfileCompositeToArrayMapper::class);
-//
-//        /**@var $IProfileMetaDataEntity IProfileMetaDataEntity */
-//        $IProfileMetaDataEntity = $this->di->make(IProfileMetaDataEntity::class);
-//        $IProfileMetaDataEntity->setMetaKey($dummyMetaDataData['key'] ?? null);
-//        $IProfileMetaDataEntity->setMetaValue($dummyMetaDataData['value'] ?? null);
-//        $profileMetaDataFillArray = [];
-//        array_push($profileMetaDataFillArray, $IProfileMetaDataEntity);
-//        $profileCompositeToArrayMapper = $ProfileCompositeToArrayMapper->mapMetaData($profileMetaDataFillArray);
-//        $this->assertCount(1, $profileCompositeToArrayMapper);
-//    }
-//
-//    public function testMapNameToArray()
-//    {
-//        $dummyNameData = $this->dummyNamesData();
-//        /**@var $ProfileCompositeToArrayMapper ProfileCompositeToArrayMapper */
-//        $ProfileCompositeToArrayMapper = $this->di->make(ProfileCompositeToArrayMapper::class);
-//
-//        /**@var $IProfileNameEntity IProfileNameEntity */
-//        $IProfileNameEntity = $this->di->make(IProfileNameEntity::class);
-//        $IProfileNameEntity->setFirst($dummyNameData['first'] ?? null);
-//        $IProfileNameEntity->setMiddle($dummyNameData['middle'] ?? null);
-//        $IProfileNameEntity->setLast($dummyNameData['last'] ?? null);
-//        $IProfileNameEntity->setPrefix($dummyNameData['prefix'] ?? null);
-//        $displayName = preg_replace('#[\s]+#', ' ', trim($IProfileNameEntity->getPrefix() . ' ' . $IProfileNameEntity->getFirst() . ' ' . $IProfileNameEntity->getMiddle() . ' ' . $IProfileNameEntity->getLast()));
-//        $profileNameFillArray = [];
-//        array_push($profileNameFillArray, $IProfileNameEntity);
-//        $profileCompositeToArrayMapper = $ProfileCompositeToArrayMapper->mapNames($profileNameFillArray);
-//        $this->assertCount(1, $profileCompositeToArrayMapper);
-//    }
+        $emailRepository = $this->di->make(IProfileEmailRepository::class);
+        $newEmailEntity =  $emailRepository->createEntity();
+        $newEmailEntity->setValidSince(!empty($dummyEmailData['valid_since']) ? new \DateTime($dummyEmailData['valid_since']) : null);
+        $newEmailEntity->setEmail($dummyEmailData['email'] ?? null);
+        $newEmailEntity->setEspDomain($dummyEmailData['esp_domain'] ?? null);
+        $newEmailEntity->setType($dummyEmailData['type'] ?? null);
+        $this->profileComposite->addEmail($newEmailEntity);
+        $this->profileComposite->setProfile($profile);
+
+        /**@var $ProfileCompositeToArrayMapper ProfileCompositeToArrayMapper */
+        $ProfileCompositeToArrayMapper = $this->di->make(ProfileCompositeToArrayMapper::class);
+        $profileCompositeToArrayMapper = $ProfileCompositeToArrayMapper->map($this->profileComposite);
+        $this->assertCount(1, $profileCompositeToArrayMapper['emails']);
+    }
+
+    public function testMapSkillToArray()
+    {
+        $dummySkillData = $this->dummySkillsData();
+        $profile = $this->profileRepository->createEntity();
+        $skillRepository = $this->di->make(IProfileSkillRepository::class);
+        $newSkillEntity =  $skillRepository->createEntity();
+        $newSkillEntity->setValidSince(!empty($dummySkillData['valid_since']) ? new \DateTime($dummySkillData['valid_since']) : null);
+        $newSkillEntity->setLevel($dummySkillData['level'] ?? null);
+        $newSkillEntity->setSkill($dummySkillData['skill'] ?? null);
+        $this->profileComposite->addSkill($newSkillEntity);
+        $this->profileComposite->setProfile($profile);
+
+        /**@var $ProfileCompositeToArrayMapper ProfileCompositeToArrayMapper */
+        $ProfileCompositeToArrayMapper = $this->di->make(ProfileCompositeToArrayMapper::class);
+
+        $profileCompositeToArrayMapper = $ProfileCompositeToArrayMapper->map($this->profileComposite);
+        $this->assertCount(1, $profileCompositeToArrayMapper['skills']);
+    }
+
+    public function testMapCriminalRecordToArray()
+    {
+        $dummyCriminalRecordData = $this->dummyCriminalRecordsData();
+        $profile = $this->profileRepository->createEntity();
+        $criminalRecordRepository = $this->di->make(IProfileCriminalRecordRepository::class);
+        $newCriminalRecordEntity =  $criminalRecordRepository->createEntity();
+        $newCriminalRecordEntity->setCaseNumber($dummyCriminalRecordData['case_number'] ?? null);
+        $newCriminalRecordEntity->setCaseType($dummyCriminalRecordData['case_type'] ?? null);
+        $newCriminalRecordEntity->setCaseYear($dummyCriminalRecordData['case_year'] ?? null);
+        $newCriminalRecordEntity->setCaseStatus($dummyCriminalRecordData['case_status'] ?? null);
+        $newCriminalRecordEntity->setDisplay($dummyCriminalRecordData['display'] ?? null);
+        $this->profileComposite->addCriminalRecord($newCriminalRecordEntity);
+        $this->profileComposite->setProfile($profile);
+
+        /**@var $ProfileCompositeToArrayMapper ProfileCompositeToArrayMapper */
+        $ProfileCompositeToArrayMapper = $this->di->make(ProfileCompositeToArrayMapper::class);
+
+        $profileCompositeToArrayMapper = $ProfileCompositeToArrayMapper->map($this->profileComposite);
+        $this->assertCount(1, $profileCompositeToArrayMapper['criminal_records']);
+    }
+
+    public function testMapMetaDataToArray()
+    {
+        $dummyMetaDataData = $this->dummyMetaData();
+        $profile = $this->profileRepository->createEntity();
+        $metaDataRepository = $this->di->make(IProfileMetaDataRepository::class);
+        $newMetaDataEntity =  $metaDataRepository->createEntity();
+        $newMetaDataEntity->setMetaKey($dummyMetaDataData['key'] ?? null);
+        $newMetaDataEntity->setMetaValue($dummyMetaDataData['value'] ?? null);
+        $this->profileComposite->addMetaData($newMetaDataEntity);
+        $this->profileComposite->setProfile($profile);
+
+        /**@var $ProfileCompositeToArrayMapper ProfileCompositeToArrayMapper */
+        $ProfileCompositeToArrayMapper = $this->di->make(ProfileCompositeToArrayMapper::class);
+        $profileCompositeToArrayMapper = $ProfileCompositeToArrayMapper->map($this->profileComposite);
+        $this->assertCount(1, $profileCompositeToArrayMapper['meta_data']);
+    }
+
+    public function testMapNameToArray()
+    {
+        $dummyNameData = $this->dummyNamesData();
+        $profile = $this->profileRepository->createEntity();
+        $nameRepository = $this->di->make(IProfileNameRepository::class);
+        $newNameEntity =  $nameRepository->createEntity();
+        $newNameEntity->setFirst($dummyNameData['first'] ?? null);
+        $newNameEntity->setMiddle($dummyNameData['middle'] ?? null);
+        $newNameEntity->setLast($dummyNameData['last'] ?? null);
+        $newNameEntity->setPrefix($dummyNameData['prefix'] ?? null);
+        $this->profileComposite->addName($newNameEntity);
+        $this->profileComposite->setProfile($profile);
+
+        /**@var $ProfileCompositeToArrayMapper ProfileCompositeToArrayMapper */
+        $ProfileCompositeToArrayMapper = $this->di->make(ProfileCompositeToArrayMapper::class);
+
+        $profileCompositeToArrayMapper = $ProfileCompositeToArrayMapper->map($this->profileComposite);
+        $this->assertCount(1, $profileCompositeToArrayMapper['names']);
+    }
 
 }
