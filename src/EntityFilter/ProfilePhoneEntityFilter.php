1c12de51f85c2c53e932a26e97992a0f53364a2b
Ahmad Saleh <ahmadfds@gmail.com>
2021-11-01 17:38:22 +0300

    entities filters

--- src/EntityFilter/ProfilePhoneEntityFilter.php
+++ src/EntityFilter/ProfilePhoneEntityFilter.php
@@ -12,6 +12,7 @@ class ProfilePhoneEntityFilter implements IProfilePhoneEntityFilter
 {
 
     protected IDependencyInjector $di;
+
     public function __construct(IDependencyInjector $di)
     {
         $this->di = $di;
@@ -19,13 +20,19 @@ class ProfilePhoneEntityFilter implements IProfilePhoneEntityFilter
 
     public function filter(IProfilePhoneEntity $entity): void
     {
-        $entity->setPossibleCountries(array_map(function($value) { return strtoupper($value);  }, $entity->getPossibleCountries()?:array($entity['country_code'])));
-        if ($entity->getOriginalNumber()){
-           $phone = $this->di->make(Phone::class);
-           $phoneParseResult = $phone->parse($entity->getOriginalNumber(),$entity->getPossibleCountries()?:array($entity['country_code']));
-           $entity->setFormattedNumber($phoneParseResult['phone']);
-           $entity->setValidPhone($phoneParseResult['is_valid']);
-           $entity->setCountryCode($phoneParseResult['country_code']);
+        $entity->setPossibleCountries(array_map(function ($value) {
+            return strtoupper($value);
+        }, $entity->getPossibleCountries()??[] ));
+
+        if ($entity->getOriginalNumber()) {
+            $countries = $entity->getPossibleCountries()??[];
+            array_unshift($countries, $entity->getCountryCode());
+            $phone = $this->di->make(Phone::class);
+            $phoneParseResult = $phone->parse($entity->getOriginalNumber(), $countries);
+
+            $entity->setFormattedNumber($phoneParseResult['phone']);
+            $entity->setValidPhone($phoneParseResult['is_valid']);
+            $entity->setCountryCode($phoneParseResult['country_code']);
         }
     }
 }
